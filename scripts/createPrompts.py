6fbde4ea54692535f822ed3e2ca01b489e6586ef
shiv10 <shivanshsharma2012@gmail.com>
2023-01-07 00:32:40 +0530

    feat: add metadata and images

---
+++ scripts/createPrompts.py
@@ -0,0 +1,33 @@
+import json
+
+elements = ['Fire','Water', 'Earth', 'Air']
+personas = ['Honesty', 'Cunning', 'Hardworking', 'Inspiring']
+traits = ['Bravery', 'Ambition', 'Empathy']
+
+names = open("names.txt", "r")
+for i in range(0, len(elements)):
+    for j in range(0, len(personas)):
+        for k in range (0, len(traits)):
+            obj = {
+                "name": names.readline().strip(),
+                "description": "NFT by cryptkiddies",
+                "image": "https://ipfs.io/ipfs/QmfU8w3TwY8Q6b9g8jebvxfnUYX2YFnvtKgNN3snQw5EVV/"+elements[i]+"/"+str(i+1)+str(j+1)+str(k+1)+".png",
+                "attributes": [
+                    {
+                        "trait_type": "Aura",
+                        "value": elements[i]
+                    },
+                    {
+                        "trait_type": "Persona",
+                        "value": personas[j]
+                    },
+                    {
+                        "trait_type": "Trait",
+                        "value": traits[k]
+                    }
+                ]
+            }
+
+            with open("metadata/"+str(i+1)+str(j+1)+str(k+1)+".json", "w") as outfile:
+                json.dump(obj, outfile)
+
